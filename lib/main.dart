6bc9d79d2c2ed05bc45ef37919643abc5dd0c8de
duncansu <duncan2001900220@gmail.com>
2022-04-22 19:17:37 +0800

    hw1

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() async {
-  runApp(
-    MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/', routes: {
-      '/': (context) => RouteOne(),
-      '/detail': (context) => RouteTwo(image: ''),
-    }),
-  );
+  runApp(Page01());
+    // MaterialApp(debugShowCheckedModeBanner: false, initialRoute: '/', routes: {
+    //   '/': (context) => RouteOne(),
+    //   '/detail': (context) => RouteTwo(image: ''),
+    // }),
+
+}
+class Page01 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Page01'),
+          ),
+          body: _Page01()
+      ),
+    );
+  }
+}
+
+
+class _Page01 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text('Go to Page02'),
+      onPressed: () {Navigator.push(context, MaterialPageRoute(builder: (context) => RouteOne()));},
+    );
+  }
 }
 
 class PhotoItem {
@@ -73,6 +99,7 @@ class RouteOne extends StatelessWidget {
 
 class RouteTwo extends StatelessWidget {
   final String image;
+  final key=GlobalKey<_ImageBrowserState>();
 
   RouteTwo({ key,  this.image})
       : super(key: key);
@@ -105,17 +132,25 @@ class RouteTwo extends StatelessWidget {
             aspectRatio: 1,
             child: Container(
               width: double.infinity,
-              child: Image(
-                image: AssetImage(image),
-              ),
+
+              child: imgBrowser
+
+
+              //     Image(
+              //   image: AssetImage(image),
+              // )
+
             ),
           ),
+
           Container(
             child: Row(
               children: <Widget>[previousBtn, nextBtn],
               mainAxisAlignment: MainAxisAlignment.center,
+
             ),
             margin: EdgeInsets.symmetric(vertical: 10),
+
           ),
         ],
       ),
@@ -127,12 +162,15 @@ class RouteTwo extends StatelessWidget {
 }
 
 
+
+
+
 class _ImageBrowser extends StatefulWidget {
   final GlobalKey<_ImageBrowserState> _key;
-  List<String> image;
+  List<String> _images;
   int _imageIndex;
 
-  _ImageBrowser(this._key, this.image) : super(key: _key) {
+  _ImageBrowser(this._key, this._images) : super(key: _key) {
     _imageIndex = 0;
   }
 
@@ -143,27 +181,24 @@ class _ImageBrowser extends StatefulWidget {
   nextImage() => _key.currentState.nextImage();
 }
 
-
-
-
 class _ImageBrowserState extends State<_ImageBrowser> {
   @override
   Widget build(BuildContext context) {
 
-    var imgs=PhotoView(enableRotation: false
-        ,backgroundDecoration:BoxDecoration(color: Colors.red),imageProvider:AssetImage(widget.image[widget._imageIndex]));
+    var imgs=PhotoView(maxScale: PhotoViewComputedScale.covered,minScale: PhotoViewComputedScale.contained*0.6,enableRotation: false
+        ,backgroundDecoration:BoxDecoration(color: Colors.red),imageProvider:AssetImage(widget._images[widget._imageIndex]));
     return imgs;
   }
 
   previousImage() {
     widget._imageIndex = widget._imageIndex == 0
-        ? widget.image.length - 1
+        ? widget._images.length - 1
         : widget._imageIndex - 1;
     setState(() {});
   }
 
   nextImage() {
-    widget._imageIndex = ++widget._imageIndex % widget.image.length;
+    widget._imageIndex = ++widget._imageIndex % widget._images.length;
     setState(() {});
   }
 }
@@ -172,64 +207,3 @@ class _ImageBrowserState extends State<_ImageBrowser> {
 
 
 
-
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: '瀏覽影像',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(),
-//     );
-//
-//   }
-// }
-//
-// class MyHomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     // 建立AppBar
-//     final appBar = AppBar(
-//       title: Text('瀏覽影像'),
-//     );
-//
-
-//
-
-//
-//
-//
-//
-//
-//     // Function to be called on click
-//     void _onTileClicked( int index){
-//       debugPrint("You tapped on item $index");
-//
-//
-//
-//       );
-//     }
-//
-// // Get grid tiles
-//     List<Widget> _getTiles(List<String> iconList) {
-//       final List<Widget> tiles = <Widget>[];
-//       for (int i = 0; i < iconList.length; i++) {
-//         tiles.add(new GridTile(
-//             child: new InkResponse(
-//               enableFeedback: true,
-//               child: new Image.asset(iconList[i], fit: BoxFit.cover,),
-//               onTap: () {
-//
-//                 ;},
-//             )
-//         )
-//         );
-//       }
-//       return tiles;
-//     }
-//   }
-// }
-//
